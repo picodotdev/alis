1571b345bc152b2eef2d17c653a5544132f7c8ea
xNNism <xnnism@gmail.com>
2019-08-24 00:01:53 +0200

    Edit to special needs

--- download.sh
+++ download.sh
@@ -15,15 +15,15 @@ rm -f alis-recovery.sh
 rm -f alis-recovery-asciinema.sh
 rm -f alis-recovery-reboot.sh
 
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis.conf
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis.sh
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-asciinema.sh
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-reboot.sh
+wget https://raw.githubusercontent.com/xnnism/alis/master/alis.conf
+wget https://raw.githubusercontent.com/xnnism/alis/master/alis.sh
+wget https://raw.githubusercontent.com/xnnism/alis/master/alis-asciinema.sh
+wget https://raw.githubusercontent.com/xnnism/alis/master/alis-reboot.sh
 
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery.conf
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery.sh
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery-asciinema.sh
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery-reboot.sh
+wget https://raw.githubusercontent.com/xnnism/alis/master/alis-recovery.conf
+wget https://raw.githubusercontent.com/xnnism/alis/master/alis-recovery.sh
+wget https://raw.githubusercontent.com/xnnism/alis/master/alis-recovery-asciinema.sh
+wget https://raw.githubusercontent.com/xnnism/alis/master/alis-recovery-reboot.sh
 
 chmod +x alis.sh
 chmod +x alis-asciinema.sh

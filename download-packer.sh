53fbac7168add165822816ac0a8f76ebf702422d
pico.dev <pico.dev@gmail.com>
2020-03-10 00:38:40 +0100

    Fixes

--- download-packer.sh
+++ download-packer.sh
@@ -5,12 +5,12 @@ wget https://raw.githubusercontent.com/picodotdev/alis/master/alis.conf
 wget https://raw.githubusercontent.com/picodotdev/alis/master/alis.sh
 wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-packer.sh
 mkdir -p packer/
-wget -o packer/alis-packer-efi-btrfs-luks-lvm-systemd.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-btrfs-luks-lvm-systemd.json
-wget -o packer/alis-packer-efi-ext4-grub-gnome.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-grub-gnome.json
-wget -o packer/alis-packer-efi-ext4-grub-kde.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-grub-kde.json
-wget -o packer/alis-packer-efi-ext4-grub-xfce.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-grub-xfce.json
-wget -o packer/alis-packer-efi-ext4-luks-lvm-grub.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-luks-lvm-grub.json
-wget -o packer/alis-packer-efi-f2fs-luks-lvm-systemd.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-f2fs-luks-lvm-systemd.json
+wget -O packer/alis-packer-efi-btrfs-luks-lvm-systemd.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-btrfs-luks-lvm-systemd.json
+wget -O packer/alis-packer-efi-ext4-grub-gnome.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-grub-gnome.json
+wget -O packer/alis-packer-efi-ext4-grub-kde.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-grub-kde.json
+wget -O packer/alis-packer-efi-ext4-grub-xfce.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-grub-xfce.json
+wget -O packer/alis-packer-efi-ext4-luks-lvm-grub.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-luks-lvm-grub.json
+wget -O packer/alis-packer-efi-f2fs-luks-lvm-systemd.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-f2fs-luks-lvm-systemd.json
 
 chmod +x ./alis-packer.sh
 ./alis-packer.sh

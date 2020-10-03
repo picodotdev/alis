dfb6626835af10af42ce301246d4c6a6b9146751
Justin Wernick <justin@worthe-it.co.za>
2020-10-03 23:06:24 +0200

    Support both i3-wm and i3-gaps as desktop environment options

--- alis.sh
+++ alis.sh
@@ -160,7 +160,7 @@ function check_variables() {
     check_variables_value "HOOKS" "$HOOKS"
     check_variables_list "BOOTLOADER" "$BOOTLOADER" "grub refind systemd"
     check_variables_list "AUR" "$AUR" "aurman yay" "false"
-    check_variables_list "DESKTOP_ENVIRONMENT" "$DESKTOP_ENVIRONMENT" "gnome kde xfce mate cinnamon lxde i3wm" "false"
+    check_variables_list "DESKTOP_ENVIRONMENT" "$DESKTOP_ENVIRONMENT" "gnome kde xfce mate cinnamon lxde i3-wm i3-gaps" "false"
     check_variables_list "DISPLAY_DRIVER" "$DISPLAY_DRIVER" "intel amdgpu ati nvidia nvidia-lts nvidia-dkms nvidia-390xx nvidia-390xx-lts nvidia-390xx-dkms nouveau" "false"
     check_variables_boolean "KMS" "$KMS"
     check_variables_boolean "FASTBOOT" "$FASTBOOT"
@@ -1267,8 +1267,11 @@ function desktop_environment() {
         "lxde" )
             desktop_environment_lxde
             ;;
-        "i3wm" )
-            desktop_environment_i3wm
+        "i3-wm" )
+            desktop_environment_i3_wm
+            ;;
+        "i3-gaps" )
+            desktop_environment_i3_gaps
             ;;
     esac
 
@@ -1305,8 +1308,13 @@ function desktop_environment_lxde() {
     arch-chroot /mnt systemctl enable lxdm.service
 }
 
-function desktop_environment_i3wm() {
-    pacman_install "i3 dmenu rxvt-unicode lightdm lightdm-gtk-greeter xorg-server"
+function desktop_environment_i3_wm() {
+    pacman_install "i3-wm i3blocks i3lock i3status dmenu rxvt-unicode lightdm lightdm-gtk-greeter xorg-server"
+    arch-chroot /mnt systemctl enable lightdm.service
+}
+
+function desktop_environment_i3_gaps() {
+    pacman_install "i3-gaps i3blocks i3lock i3status dmenu rxvt-unicode lightdm lightdm-gtk-greeter xorg-server"
     arch-chroot /mnt systemctl enable lightdm.service
 }
 

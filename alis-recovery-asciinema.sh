e795fc7d644431d9ed317b529edf8f10eae835af
pico.dev <pico.dev@gmail.com>
2020-11-03 22:42:48 +0100

    Asciinema changes

--- alis-recovery-asciinema.sh
+++ alis-recovery-asciinema.sh
@@ -6,7 +6,6 @@ set -e
 # Copyright (C) 2020 picodotdev
 
 curl -o asciinema-2.0.2.zip https://github.com/asciinema/asciinema/archive/v2.0.2.zip
-bsdtar -x -f asciinema-2.0.2.zip
-cp -r asciinema-2.0.2/* .
+unzip asciinema-2.0.2.zip
 rm -f alis.asciinema
-python3 -m asciinema rec -i 5 ~/alis-recovery.asciinema
+(cd asciinema-2.0.2 && python3 -m asciinema rec -i 5 ~/alis.asciinema)

ecfdb60c52e604a972d5694bda23a27bf05a23a6
pico.dev <pico.dev@gmail.com>
2020-11-03 20:04:40 +0100

    Asciinema changes

--- alis-asciinema.sh
+++ alis-asciinema.sh
@@ -6,7 +6,6 @@ set -e
 # Copyright (C) 2020 picodotdev
 
 curl -L -o asciinema-2.0.2.zip https://github.com/asciinema/asciinema/archive/v2.0.2.zip
-bsdtar -x -f asciinema-2.0.2.zip
-cp -r asciinema-2.0.2/* .
+unzip asciinema-2.0.2.zip
 rm -f alis.asciinema
-python3 -m asciinema rec -i 5 ~/alis.asciinema
+(cd asciinema-2.0.2 && python3 -m asciinema rec -i 5 ~/alis.asciinema)

7e3269e30f259fa4ef4f1153bcf156cb6c934abc
pico.dev <pico.dev@gmail.com>
2021-08-12 18:19:26 +0200

    Install a more reduced set of applications for KDE

--- alis.sh
+++ alis.sh
@@ -1595,7 +1595,7 @@ function desktop_environment_gnome() {
 }
 
 function desktop_environment_kde() {
-    pacman_install "plasma-meta plasma-wayland-session kde-system-meta kde-utilities-meta kde-graphics-meta kde-multimedia-meta"
+    pacman_install "plasma-meta plasma-wayland-session kde-system-meta kde-utilities-meta kde-graphics-meta kde-multimedia-meta kde-network-meta"
     arch-chroot /mnt systemctl enable sddm.service
 }
 

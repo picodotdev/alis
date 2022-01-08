c6fb7c8030a331715576d2103a8a47495b60cd9d
Erik Dubois <erik.dubois@gmail.com>
2022-01-08 17:53:24 +0100

    awesome and qtile

--- alis.sh
+++ alis.sh
@@ -1727,14 +1727,15 @@ function desktop_environment_bspwm() {
 }
 
 function desktop_environment_awesome() {
-    pacman_install "awesome vicious xterm lightdm lightdm-gtk-greeter"
+    pacman_install "awesome vicious xterm lightdm lightdm-gtk-greeter xorg-server"
     arch-chroot /mnt systemctl enable lightdm.service
 }
 
 function desktop_environment_qtile() {
-    pacman_install "qtile xterm lightdm lightdm-gtk-greeter"
+    pacman_install "qtile xterm lightdm lightdm-gtk-greeter xorg-server"
     arch-chroot /mnt systemctl enable lightdm.service
 }
+
 
 function packages() {
     if [ "$PACKAGES_INSTALL" == "true" ]; then

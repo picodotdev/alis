fdb2e13ddc9a44f1bb9403a0fdb614346bb248ec
arcolinuxz <arcolinuxinfo@gmail.com>
2022-01-23 06:35:06 +0100

    leftwm and dusk

--- README.md
+++ README.md
@@ -55,7 +55,7 @@ For new features, improvements and bugs fill an issue in GitHub or make a pull r
 * **Partition**: no LVM, LVM, LVM on LUKS, GPT on UEFI, MBR on BIOS
 * **File system**: ext4, btrfs (with subvols), xfs, f2fs, reiserfs
 * **Kernels**: linux, linux-lts, linux-hardened, linux-zen
-* **Desktop environment**: GNOME, KDE, XFCE, Mate, Cinnamon, LXDE, i3-wm, i3-gaps, Deepin, Budgie, Bspwm, Awesome, Qtile, Openbox
+* **Desktop environment**: GNOME, KDE, XFCE, Mate, Cinnamon, LXDE, i3-wm, i3-gaps, Deepin, Budgie, Bspwm, Awesome, Qtile, Openbox, Leftwm, Dusk
 * **Display managers**: GDM, SDDM, Lightdm, lxdm
 * **Graphics controller**: intel, nvidia and amd with optionally early KMS start. With intel optionally fastboot, hardware video acceleration and framebuffer compression.
 * **Bootloader**: GRUB, rEFInd, systemd-boot
@@ -241,6 +241,7 @@ https://www.archlinux.org/download/
 * https://wiki.archlinux.org/title/Dm-crypt
 * https://wiki.archlinux.org/title/Dm-crypt/Device_encryption
 * https://wiki.archlinux.org/title/Dm-crypt/Encrypting_an_entire_system
+* https://github.com/bakkeby/dusk
 * https://wiki.archlinux.org/title/EFI_System_Partition
 * https://wiki.archlinux.org/title/Ext4
 * https://wiki.archlinux.org/title/F2FS
@@ -258,6 +259,7 @@ https://www.archlinux.org/download/
 * https://wiki.archlinux.org/title/Kernel_mode_setting
 * https://wiki.archlinux.org/title/Kernel_parameters
 * https://wiki.archlinux.org/title/Kernels
+* https://wiki.archlinux.org/title/LeftWM
 * https://wiki.archlinux.org/title/List_of_applications
 * https://wiki.archlinux.org/title/LVM
 * https://wiki.archlinux.org/title/Main_page
--- alis.sh
+++ alis.sh
@@ -147,7 +147,7 @@ function check_variables() {
     check_variables_value "HOOKS" "$HOOKS"
     check_variables_list "BOOTLOADER" "$BOOTLOADER" "auto grub refind systemd" "true" "true"
     check_variables_list "CUSTOM_SHELL" "$CUSTOM_SHELL" "bash zsh dash fish" "true" "true"
-    check_variables_list "DESKTOP_ENVIRONMENT" "$DESKTOP_ENVIRONMENT" "gnome kde xfce mate cinnamon lxde i3-wm i3-gaps deepin budgie bspwm awesome qtile openbox" "false" "true"
+    check_variables_list "DESKTOP_ENVIRONMENT" "$DESKTOP_ENVIRONMENT" "gnome kde xfce mate cinnamon lxde i3-wm i3-gaps deepin budgie bspwm awesome qtile openbox leftwm dusk" "false" "true"
     check_variables_boolean "PACKAGES_MULTILIB" "$PACKAGES_MULTILIB"
     check_variables_boolean "PACKAGES_INSTALL" "$PACKAGES_INSTALL"
     check_variables_boolean "PROVISION" "$PROVISION"
@@ -1475,6 +1475,12 @@ function desktop_environment() {
             ;;
         "openbox" )
             desktop_environment_openbox
+                ;;
+        "dusk" )
+                desktop_environment_dusk
+                ;;
+        "leftwm" )
+            desktop_environment_leftwm
             ;;
     esac
 
@@ -1549,6 +1555,19 @@ function desktop_environment_qtile() {
 
 function desktop_environment_openbox() {
     pacman_install "openbox obconf xterm lightdm lightdm-gtk-greeter xorg-server"
+    arch-chroot /mnt systemctl enable lightdm.service
+}
+
+function desktop_environment_dusk() {
+    #dusk-git in AUR - set in alis-packages.conf
+    pacman_install "dmenu xterm lightdm lightdm-gtk-greeter xorg-server"
+    arch-chroot /mnt systemctl enable lightdm.service
+}
+
+function desktop_environment_leftwm() {
+    #leftwm-git in AUR - set in alis-packages.conf
+    #leftwm-theme-git in AUR - set in alis-packages.conf
+    pacman_install "dmenu xterm lightdm lightdm-gtk-greeter xorg-server"
     arch-chroot /mnt systemctl enable lightdm.service
 }
 

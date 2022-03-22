adfc7daa7e777eab45e6e954cff695cc7e904197
pico.dev <pico.dev@gmail.com>
2022-03-22 21:54:25 +0100

    Support download a specific alis version based on the commit hash

--- download-sid.sh
+++ download-sid.sh
@@ -6,10 +6,17 @@ set -eu
 # Copyright (C) 2022 picodotdev
 
 GITHUB_USER="picodotdev"
-BRANCH="sid"
+BRANCH="master"
+HASH=""
 
-while getopts "u:" arg; do
+while getopts "b:h:u:" arg; do
   case ${arg} in
+    b)
+      BRANCH="${OPTARG}"
+      ;;
+    h)
+      HASH="${OPTARG}"
+      ;;
     u)
       GITHUB_USER=${OPTARG}
       ;;
@@ -21,9 +28,15 @@ while getopts "u:" arg; do
 done
 
 set -o xtrace
-curl -sL -o "alis-$BRANCH.zip" https://github.com/$GITHUB_USER/alis/archive/refs/heads/$BRANCH.zip
-bsdtar -x -f "alis-$BRANCH.zip"
-cp -R alis-$BRANCH/*.sh alis-$BRANCH/*.conf alis-$BRANCH/files/ alis-$BRANCH/configs/ ./
+if [ -n "$HASH" ]; then
+  curl -sL -o "alis-$HASH.zip" https://github.com/$GITHUB_USER/alis/archive/$HASH.zip
+  bsdtar -x -f "alis-$HASH.zip"
+  cp -R alis-$HASH/*.sh alis-$HASH/*.conf alis-$HASH/files/ alis-$HASH/configs/ ./
+else
+  curl -sL -o "alis-$BRANCH.zip" https://github.com/$GITHUB_USER/alis/archive/refs/heads/$BRANCH.zip
+  bsdtar -x -f "alis-$BRANCH.zip"
+  cp -R alis-$BRANCH/*.sh alis-$BRANCH/*.conf alis-$BRANCH/files/ alis-$BRANCH/configs/ ./
+fi
 chmod +x configs/*.sh
 chmod +x *.sh
 
--- download.sh
+++ download.sh
@@ -7,9 +7,16 @@ set -eu
 
 GITHUB_USER="picodotdev"
 BRANCH="master"
+HASH=""
 
-while getopts "u:" arg; do
+while getopts "b:h:u:" arg; do
   case ${arg} in
+    b)
+      BRANCH="${OPTARG}"
+      ;;
+    h)
+      HASH="${OPTARG}"
+      ;;
     u)
       GITHUB_USER=${OPTARG}
       ;;
@@ -21,8 +28,14 @@ while getopts "u:" arg; do
 done
 
 set -o xtrace
-curl -sL -o "alis-$BRANCH.zip" https://github.com/$GITHUB_USER/alis/archive/refs/heads/$BRANCH.zip
-bsdtar -x -f "alis-$BRANCH.zip"
-cp -R alis-$BRANCH/*.sh alis-$BRANCH/*.conf alis-$BRANCH/files/ alis-$BRANCH/configs/ ./
+if [ -n "$HASH" ]; then
+  curl -sL -o "alis-$HASH.zip" https://github.com/$GITHUB_USER/alis/archive/$HASH.zip
+  bsdtar -x -f "alis-$HASH.zip"
+  cp -R alis-$HASH/*.sh alis-$HASH/*.conf alis-$HASH/files/ alis-$HASH/configs/ ./
+else
+  curl -sL -o "alis-$BRANCH.zip" https://github.com/$GITHUB_USER/alis/archive/refs/heads/$BRANCH.zip
+  bsdtar -x -f "alis-$BRANCH.zip"
+  cp -R alis-$BRANCH/*.sh alis-$BRANCH/*.conf alis-$BRANCH/files/ alis-$BRANCH/configs/ ./
+fi
 chmod +x configs/*.sh
 chmod +x *.sh

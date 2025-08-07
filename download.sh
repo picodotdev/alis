ed7f4a0ab9d70dfd12545742ebfa63adda3e3a27
libertine89 <38113173+libertine89@users.noreply.github.com>
2025-08-07 19:20:19 +0100

    configured variables

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ set -eu
 # and customized Arch Linux system.
 # Copyright (C) 2022 picodotdev
 
-GITHUB_USER="picodotdev"
+GITHUB_USER="libertine89"
 BRANCH="main"
 HASH=""
 ARTIFACT="alis-${BRANCH}"
